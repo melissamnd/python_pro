1742ca4f6c918bf1bee808990bf8d3696c6f5251
Melissa Mesnard <cloe.mesnard@gmail.com>
2025-01-14 13:52:31 +0100

    Updating function

---
+++ src/python_pro/risk.models.py
@@ -0,0 +1,33 @@
+"""
+- Sample covariance
+- Cov to correlation functipn
+"""
+import numpy as np
+import pandas as pd
+
+from .expected_returns import returns_from_prices
+
+def sample_cov(prices, returns_data=False, frequency=252, log_returns=False, **kwargs):
+
+    if not isinstance(prices, pd.DataFrame):
+        warnings.warn("data is not in a dataframe", RuntimeWarning)
+        prices = pd.DataFrame(prices)
+    if returns_data:
+        returns = prices
+    else:
+        returns = returns_from_prices(prices, log_returns)
+    return fix_nonpositive_semidefinite(
+        returns.cov() * frequency, kwargs.get("fix_method", "spectral")
+    )
+
+
+def cov_to_corr(cov_matrix):
+
+    if not isinstance(cov_matrix, pd.DataFrame):
+        warnings.warn("cov_matrix is not a dataframe", RuntimeWarning)
+        cov_matrix = pd.DataFrame(cov_matrix)
+
+    Dinv = np.diag(1 / np.sqrt(np.diag(cov_matrix)))
+    corr = np.dot(Dinv, np.dot(cov_matrix, Dinv))
+    return pd.DataFrame(corr, index=cov_matrix.index, columns=cov_matrix.index)
+
