39b10544aaf55d57e525abc5fbb325ded96db573
Melissa Mesnard <cloe.mesnard@gmail.com>
2025-01-15 19:38:23 +0100

    Adding dynamic variables

--- dev.py
+++ dev.py
@@ -1,35 +1,41 @@
-from pybacktestchain.broker import StopLoss
+#### MY DEV.PY FILE
+
+from pybacktestchain.data_module import FirstTwoMoments
+#from pybacktestchain.broker import StopLoss  
 from pybacktestchain.blockchain import load_blockchain
-from src.python_pro.new_broker import Backtest
-from src.python_pro.Interactive_inputs import get_initial_cash_input, get_date_inputs, get_rebalancing_strategy, get_target_return
+from datetime import datetime
 
-def algo_backtest():
-    # Set verbosity for logging
-    verbose = False  # Set to True to enable logging, or False to suppress it
+# Set verbosity for logging
+verbose = False  # Set to True to enable logging, or False to suppress it
 
-    initial_cash, threshold, start_date, end_date = get_initial_parameter()
-    rebalancing_strategy = strategy_choice()
-    ask_user_for_comment()
+# Get initial inputs from the user
+start_date, end_date = get_date_inputs()  # Get start and end date from the user input
+stop_loss_threshold = get_stop_loss_threshold()  # Get the stop-loss threshold
+rebalancing_strategy = get_rebalancing_strategy()  # Get the rebalancing strategy
+initial_cash = get_initial_cash_input()  # Get the initial cash amount
+tickers = get_stock_inputs()  # Get the list of stock tickers
 
+rebalancing_strategy_instance = rebalancing_strategy()
 
-    backtest = Backtest(initial_date=start_date,
-            final_date=end_date,
-            initial_cash=initial_cash,
-            threshold = threshold,
-            information_class=FirstTwoMoments,
-            strategy_name=rebalancing_strategy,
-            risk_model=StopLoss,
-            name_blockchain='backtest',
-            verbose=verbose
-            )
-    backtest.run_backtest()
+# Now we need to use the Backtest class, correctly passing the parameters.
+backtest = Backtest2(
+    initial_date=start_date,         # Start date for the backtest (DYNAMIC)      
+    final_date=end_date,             # End date for the backtest (DYNAMIC)
+    threshold=stop_loss_threshold,   # The stop-loss threshold value (DYNAMIC)
+    information_class=FirstTwoMoments,          # The class used to calculate portfolio information
+    risk_model=StopLoss_new,                    # The risk model used (e.g., Modified version of StopLoss)
+    name_blockchain='backtest',             # The name of the blockchain where results are stored
+    initial_cash=initial_cash,  # Initial amount of cash in the portfolio (DYNAMIC)
+    universe=tickers,  # The stock tickers provided by the user
+    rebalance_flag=rebalancing_strategy_instance,  # The rebalancing strategy to be used
+    verbose=verbose                        # Whether to show logs or not
+)
 
+# Run the backtest
+backtest.run_backtest()
 
-    block_chain = load_blockchain('backtest')
-    print(str(block_chain))
-    # check if the blockchain is valid
-    print("Blockchain valid:", block_chain.is_valid())
-
-if __name__ == "__main__":
-    algo_backtest()
-
+# Load the blockchain to check the results
+block_chain = load_blockchain('backtest')
+print(str(block_chain)) # Print the blockchain content (backtest results)
+# Check if the blockchain is valid
+print(block_chain.is_valid())
